28b6ec1e6d82d57a28ba26fe2f6cc38ff2dd5fa9
Erik Jones <ejones99@umd.edu>
2024-04-26 14:37:51 -0400

    Refactored index_search on Query class.
    Refactored the index_search argument/property on the Query class
    to match LOC standard, which would call it a 'cql query'. In line
    with that it is now called 'cql_query.'

--- src/sru_queryer/_base/_query.py
+++ src/sru_queryer/_base/_query.py
@@ -11,9 +11,9 @@ from ._sort_key import SortKey
 
 class Query:
 
-    def __init__(self, sru_configuration: SRUConfiguration, index_search: IndexQuery | CQLBooleanOperatorBase | LITERAL, start_record: int | None = None, maximum_records: int | None = None, record_schema: str | None = None, sort_queries: list[dict] | list[SortKey] | None = None, record_packing: str | None = None):
+    def __init__(self, sru_configuration: SRUConfiguration, cql_query: IndexQuery | CQLBooleanOperatorBase | LITERAL, start_record: int | None = None, maximum_records: int | None = None, record_schema: str | None = None, sort_queries: list[dict] | list[SortKey] | None = None, record_packing: str | None = None):
         self.sru_configuration = sru_configuration
-        self.index_search = index_search
+        self.cql_query = cql_query
         self.start_record = start_record
         self.maximum_records = maximum_records
         self.record_schema = record_schema
@@ -34,7 +34,7 @@ class Query:
         SRUValidator.validate_base_query(self.sru_configuration,
             self.start_record, self.maximum_records, self.record_schema, self.record_packing)
 
-        self.index_search.validate(self.sru_configuration)
+        self.cql_query.validate(self.sru_configuration)
 
         SRUValidator.validate_sort(self.sru_configuration, self.sort_queries, self.record_schema)
 
@@ -52,10 +52,10 @@ class Query:
         search_retrieve_query = SRUAuxiliaryFormatter.format_base_search_retrieve_query(self.sru_configuration,
             self.start_record, self.maximum_records, self.record_schema, self.record_packing)
 
-        if isinstance(self.index_search, IndexQuery) and not (self.index_search.get_index_name() and self.index_search.get_operation()):
+        if isinstance(self.cql_query, IndexQuery) and not (self.cql_query.get_index_name() and self.cql_query.get_operation()):
             # If it's just a single value (search term) as the query, without anything else, append an equals sign.
             search_retrieve_query += "="
-        search_retrieve_query += self.index_search.format()
+        search_retrieve_query += self.cql_query.format()
 
         search_retrieve_query += SRUAuxiliaryFormatter.format_sort_query(self.sort_queries)
 
